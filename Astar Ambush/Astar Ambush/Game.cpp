cf3bf9c0ade0c9b3cd2b35a52f1df72054cf9036
keithw96 <c00205321@itcarlow.ie>
2019-03-26 21:42:34 +0000

    sdl and sdl image implemented

---
+++ Astar Ambush/Astar Ambush/Game.cpp
@@ -0,0 +1,22 @@
+#include "Game.h"
+
+
+void Game::initialise()
+{
+
+}
+
+void Game::run()
+{
+
+}
+
+void Game::update()
+{
+
+}
+
+void Game::render()
+{
+
+}
---
+++ Astar Ambush/Astar Ambush/Game.h
@@ -0,0 +1,13 @@
+#pragma once
+class Game
+{
+public:
+
+	void initialise();
+	void run();
+	void update();
+	void render();
+
+private:
+
+};
